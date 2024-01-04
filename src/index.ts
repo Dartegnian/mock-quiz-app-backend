6e53c712798f5a0569c1614c452ad5efe1969419
Dartegnian <root@dartegnian.com>
2024-01-05 00:03:49 +0800

    Install MongoDB, connect to sample DB

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
 import router from './router';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -23,3 +24,8 @@ const server = http.createServer(app);
 server.listen(8080, () => {
 	console.log("Quiz BE on port 8080");
 });
+
+const MONGO_URL = "mongodb+srv://XXXXXXXXXXXXXXXXXXX:XXXXXXXXXXXXXXXXXXX@mockquizcluster.xhvsuyr.mongodb.net/?retryWrites=true&w=majority"
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.error(error));
