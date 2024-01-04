34259c982835d01038e029a8da14ea29e05b8624
Dartegnian <root@dartegnian.com>
2024-01-04 23:19:00 +0800

    Start simple express backend with empty route

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,25 @@
-const helloMessage = "sanity test mock quiz backend";
-console.log(helloMessage);
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+import router from './router';
+
+const app = express();
+
+app.use(cors({
+	credentials: true
+}))
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+app.use('/', router);
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+	console.log("Quiz BE on port 8080");
+});
