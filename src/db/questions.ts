0b8fa77bc24b3cafe0ef21ee5872eefc784708a6
Dartegnian <root@dartegnian.com>
2024-01-06 00:43:17 +0800

    Don't use UserModel in DB transaction, write new func for saving questions

--- src/db/questions.ts
+++ src/db/questions.ts
@@ -1,6 +1,5 @@
 import mongoose, { Schema } from "mongoose";
 import IQuizQuestion from "../interfaces/quiz-question.interface";
-import { UserModel } from "./users";
 
 
 interface IQuizQuestionDocument extends IQuizQuestion, Document { }
@@ -15,7 +14,15 @@ const QuizQuestionSchema = new mongoose.Schema({
 
 export const QuizQuestionModel = mongoose.model<IQuizQuestionDocument>('QuizQuestion', QuizQuestionSchema);
 
-export const getQuestions = () => UserModel.find();
-export const createQuestion = (values: Record<string, any>) => new QuizQuestionModel(values).save().then((question) => question.toObject());
-export const deleteQuestionById = (id: string) => UserModel.findByIdAndDelete(id);
-export const updateQuestionById = (id: string, values: Record<string, any>) => UserModel.findByIdAndUpdate(id, values);
+export const getQuestions = () => QuizQuestionModel.find();
+export const createQuestion = async (values: IQuizQuestion): Promise<IQuizQuestionDocument> => {
+	try {
+		const question = await new QuizQuestionModel(values).save();
+		return question.toObject();
+	} catch (error) {
+		console.error(error);
+		throw new Error('Failed to create question');
+	}
+};
+export const deleteQuestionById = (id: string) => QuizQuestionModel.findByIdAndDelete(id);
+export const updateQuestionById = (id: string, values: Record<string, any>) => QuizQuestionModel.findByIdAndUpdate(id, values);
