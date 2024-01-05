92b7b1086817553d449e63040a41b3146f7ef90f
Dartegnian <root@dartegnian.com>
2024-01-06 00:43:45 +0800

    Handle new question creation in controller

--- src/controllers/questions.ts
+++ src/controllers/questions.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { deleteQuestionById, getQuestions } from '../db/questions';
+import { deleteQuestionById, getQuestions, createQuestion } from '../db/questions';
 
 export const getAllQuestions = async (req: express.Request, res: express.Response) => {
 	try {
@@ -17,13 +17,38 @@ export const deleteQuestion = async (req: express.Request, res: express.Response
 		const { id } = req.params;
 
 		const deletedQuestion = await deleteQuestionById(id);
-		
+
 		if (!deletedQuestion) {
 			console.error(deletedQuestion);
 			return res.sendStatus(400);
 		}
 
 		return res.json(deletedQuestion);
+	} catch (error) {
+		console.error(error);
+		return res.sendStatus(400);
+	}
+}
+
+export const createNewQuestion = async (req: express.Request, res: express.Response) => {
+	try {
+		const { type, question, answer, pointGrade, choices } = req.body;
+		console.warn(req.body);
+
+		if (!type || !question || !answer || !pointGrade || !choices) {
+			console.error("Incomplete question");
+			return res.sendStatus(400);
+		}
+
+		const createdQuestion = await createQuestion({
+			type,
+			question,
+			answer,
+			pointGrade,
+			choices
+		});
+
+		return res.status(200).json(createdQuestion).end();
 	} catch (error) {
 		console.error(error);
 		return res.sendStatus(400);
